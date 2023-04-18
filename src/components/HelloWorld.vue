13542bdead0b14476fde24c83bb5fc90d637c82b
lesly <dearlulu1995@gmail.com>
2023-04-18 16:16:55 +0800

    页面抖动

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h2>this is my website</h2>
+    <h1>Welcome to My Vlog Today</h1>
+    <h2>-- from 甜辣酱 2023/04/18</h2>
   </div>
 </template>
 
@@ -11,9 +12,76 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h2 {
-  margin: 40px 0 0;
-  color: #42b983;
+.hello{
+  background-color: pink;
+  border-radius: 40px;
+  margin: 40px;
 }
+
+h1 {
+  margin: 80px;
+  padding: 180px 60px;
+  color: rgb(124, 165, 241);
+  font-size: 84px;
+}
+
+h1:hover {
+  cursor: pointer;
+  animation: animate 0.8s linear infinite;
+}
+
+h2{
+  cursor: pointer;
+  padding-bottom: 100px;
+  animation: rotate 2s linear infinite;
+}
+
+@keyframes animate {
+0%,
+25% {
+    text-shadow: 6px 6px 6px #f34f72aa;
+}
+
+50% {
+  text-shadow: 6px -6px 6px #f34f72aa;
+}
+
+75% {
+  text-shadow: -6px 6px 6px #f34f72aa;
+}
+
+100% {
+  text-shadow: -6px -6px 6px #f34f72aa;
+}
+}
+
+@keyframes rotate {
+10%,
+90% {
+    transform: translate3d(-1.2px, 0, 0);
+}
+
+20%,
+80% {
+    transform: translate3d(2px, 0, 0);
+}
+
+30%,
+70% {
+    transform: translate3d(-4.8px, 0, 0);
+}
+
+40%,
+60% {
+    transform: translate3d(4.8px, 0, 0);
+}
+
+50% {
+    transform: translate3d(-4.8px, 0, 0);
+}
+
+
+}
+
 
 </style>
